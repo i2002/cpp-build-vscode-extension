671a2694c965c3f1f12d19f12833e2fea4e35349
T B <inventator2002.aa@gmail.com>
2019-11-27 20:33:36 +0200

    fix canvas issue mock canvas module so that it can be imported

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ const config = {
   devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    canvas: 'commonjs canvas'
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
